ce997ca2cafcf0fba9d642e9f69d8cf4e9fdbb35
Bam1594 <59908410+Bam1594@users.noreply.github.com>
2020-03-24 09:28:51 +0700

    commit

---
+++ index.php
@@ -0,0 +1,11 @@
+< HTML >
+< HEAD > < TITLE >หน้าแรกของฉัน</ TITLE > </ HEAD >
+< BODY  BGCOLOR = #FFFFFF >
+< H1 >
+<? php  echo  "รหัสนิสิต - 6231023" ; ?> < br >
+<? php  echo  "ชื่อ - สกุล นายพิชญา สีแดง " ; ?> < br >
+< iframe  width = " 500 " height = " 500 "
+src = " https://www.youtube.com/embed/RgKAFK5djSk?autoplay=1 " >
+</ iframe >
+</ BODY >
+</ HTML >
