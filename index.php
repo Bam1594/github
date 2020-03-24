706621692ca83e48e968567e8de8f678feda9d16
Bam1594 <59908410+Bam1594@users.noreply.github.com>
2020-03-24 09:42:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 <? php  echo  "รหัสนิสิต - 6231023" ; ?> < br >
 <? php  echo  "ชื่อ - สกุล นายพิชญา สีแดง " ; ?> < br >
 < iframe  width = " 500 " height = " 500 "
+src = " https://youtu.be/7VAUXffyF0A " >
 </ iframe >
 </ BODY >
 </ HTML >
